f386ad2e068d90ceae1c70b1a99311cd1db959fe
IsaiasMontielBravo <isimetallica@hotmail.com>
2018-03-22 15:05:13 +0100

    s

---
+++ CrudAngular5/angular-crud-firebase/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCaJLm-bX_QeDYXp86zOatXvDt2YDdTjeU",
+    authDomain: "angular-crud-firebase-fcb82.firebaseapp.com",
+    databaseURL: "https://angular-crud-firebase-fcb82.firebaseio.com",
+    projectId: "angular-crud-firebase-fcb82",
+    storageBucket: "angular-crud-firebase-fcb82.appspot.com",
+    messagingSenderId: "891994749207"
+  }
+};
